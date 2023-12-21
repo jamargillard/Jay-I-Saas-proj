a31021ee5a7bf74ea291633c3e2ba32a3f91e6ea
jamar gillard <jaydevelopment101@gmail.com>
2023-12-21 10:00:31 -0500

    test 2 new node

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,12 +2,10 @@ import Stripe from "stripe"
 import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 
-   
 import prismadb from "@/lib/prismadb"
 import { stripe } from "@/lib/stripe"
 
 export async function POST(req: Request) {
-
   const body = await req.text()
   const signature = headers().get("Stripe-Signature") as string
 
