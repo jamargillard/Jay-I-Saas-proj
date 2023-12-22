e81ea01f8f7139c75dadaebcdf6bc38ce5a4f600
jamar gillard <jaydevelopment101@gmail.com>
2023-12-21 19:26:10 -0500

    next update?

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,6 +3,7 @@ import { headers } from "next/headers"
 import { NextResponse } from "next/server"
 
 import prismadb from "@/lib/prismadb"
+  
 import { stripe } from "@/lib/stripe"
 
 export async function POST(req: Request) {
